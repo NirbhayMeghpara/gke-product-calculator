2648d973192ddb9b3795a0f2c198c967103831de
Nirbhay Meghpara <nirbhaymeghpara123@gmail.com>
2025-03-10 23:14:49 -0300

    fix: remove unnecessary semicolons in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express'
-import bodyParser from 'body-parser';
-import sumRoute from './routes/sum.route';
+import bodyParser from 'body-parser'
+import sumRoute from './routes/sum.route'
 
 const app = express();
 const PORT = process.env.PORT || 3000;
